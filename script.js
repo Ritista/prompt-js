db33e386f5d982a9e0ea3e582698be090c489743
Ritista Shrestha <ritista99@gmail.com>
2024-06-13 14:45:20 +0545

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//condition
-//if, else
-//if(condition){
-//    statement
-//}
-a=2;
-console.log(a==2)
-if(a==true){
-  console.log("i am a girl")
-}
+document.getElementById("hello").textContent="this is my day 1 js file";
+
+let a = prompt("what is your name");
+console.log(typeof a);
+let b = Number(prompt("enter first number"));
+console.log(typeof b);
+let c = Number(prompt("enter second number"));
+console.log(typeof c);
+let d = b+c;
+
+document.getElementById("name").textContent= `my name is ${a}`;
+document.getElementById("address").textContent= `the number is ${b} and ${c} sum is ${d}`;
